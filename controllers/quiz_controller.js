5ab6e6609ef095c3a590f3c034d9973378941d13
Jose Ganchozo <joseganchozo@outlook.com>
2015-07-26 20:17:27 -0500

    Primera Pregunta Corregido

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,10 +1,10 @@
 //GET /quizes/question
-export.question = function(req, res){
+exports.question = function(req, res){
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
 //GET /quizes/question
-export.answer = function(req, res){
+exports.answer = function(req, res){
 if(req.query.respuesta==="Roma"){
 		res.render('quizes/answer', {respuesta: 'Correcto'});
 }else{
